2c6ecf14d25b68fdb1ebb6826c751b7b832ddbed
Ltcc79 <teikchoonlow@gmail.com>
2025-12-06 15:56:30 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c4nhsydqvt33.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Bait3723"
+customdb = "employee_db"
+custombucket = "ltccccc-bucket"
+customregion = "us-east-1"
+
